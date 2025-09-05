4006ff19778a0979487e605560a298dbe73e39e3
Meena Atai <2397995@cmontmorency.qc.ca>
2025-09-05 13:15:45 -0400

    new code

--- tp1-scene_reactive/scenes/button.gd
+++ tp1-scene_reactive/scenes/button.gd
@@ -1,13 +1,10 @@
 extends Button
 
-@export var sprite: AnimatedSprite2D
-@export var sound: AudioStreamPlayer
-
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		$AnimatedSprite2D.play()
+		$CharacterBody2D/AnimatedSprite2D.play()
 		$AudioStreamPlayer.play()
 	else:
-		$AnimatedSprite2D.stop()
+		$CharacterBody2D/AnimatedSprite2D.stop()
 		$AudioStreamPlayer.stop()
  
