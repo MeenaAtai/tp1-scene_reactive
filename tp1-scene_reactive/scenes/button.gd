6b5aaf0030a0860447699d8d94268a24adfb773c
Meena Atai <2397995@cmontmorency.qc.ca>
2025-09-05 11:48:03 -0400

    amelioration du script

--- tp1-scene_reactive/scenes/button.gd
+++ tp1-scene_reactive/scenes/button.gd
@@ -1,4 +1,10 @@
 extends Button
 
-func _on_pressed() -> void:
-	$AudioStreamPlayer.play()
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$AnimatedSprite2D.play()
+		$AudioStreamPlayer.play()
+	else:
+		$AnimatedSprite2D.stop()
+		$AudioStreamPlayer.stop()
+	pass 
--- tp1-scene_reactive/scenes/button_2.gd
+++ tp1-scene_reactive/scenes/button_2.gd
@@ -3,5 +3,7 @@ extends Button
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		$AudioStreamPlayer.play()
+		$AnimatedSprite2D.play()
 	else:
 		$AudioStreamPlayer.stop()
+		$AnimatedSprite2D.stop()
