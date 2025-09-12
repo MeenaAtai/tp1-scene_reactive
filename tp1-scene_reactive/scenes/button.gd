784e37406ba8748861c936c9879e3f9a3f5d491a
Meena Atai <meenaatai2005@gmail.com>
2025-09-12 06:50:45 -0400

    export html5

--- tp1-scene_reactive/scenes/button.gd
+++ tp1-scene_reactive/scenes/button.gd
@@ -1,16 +1,16 @@
 extends Button
 
-var _button_pressed: bool = false
-var _attacking: bool = false
+var running: bool = false
+var attacking: bool = false
 
 func _ready() -> void:
 	toggle_mode = true
 	toggled.connect(_on_toggled)
 
 func _on_toggled(toggled_on: bool) -> void:
-	_button_pressed = toggled_on
+	running = toggled_on
 	
-	if toggled_on and not _attacking:
+	if running and not attacking:
 		$CharacterBody2D/AnimatedSprite2D.play("run")
 		$AudioStreamPlayer.play()
 	else:
@@ -18,20 +18,20 @@ func _on_toggled(toggled_on: bool) -> void:
 		$AudioStreamPlayer.stop()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left"):
-		if not _attacking:
+	if event.is_action_pressed("touche left"):
+		if not attacking:
 			_start_attack()
 		else:
 			_stop_attack()
 
 func _start_attack() -> void:
-	_attacking = true
-	$CharacterBody2D/AnimatedSprite2D.play("left")
+	attacking = true
+	$CharacterBody2D/AnimatedSprite2D.play("attack")
 	$AudioStreamPlayer.play()
 
 func _stop_attack() -> void:
-	_attacking = false
-	if _button_pressed:
+	attacking = false
+	if running: 
 		$CharacterBody2D/AnimatedSprite2D.play("run")
 	else:
 		$CharacterBody2D/AnimatedSprite2D.stop()
--- tp1-scene_reactive/scenes/button_2.gd
+++ tp1-scene_reactive/scenes/button_2.gd
@@ -1,16 +1,16 @@
 extends Button
 
-var _button_pressed: bool = false
-var _attacking: bool = false
+var running: bool = false
+var jumping: bool = false
 
 func _ready() -> void:
 	toggle_mode = true
 	toggled.connect(_on_toggled)
 
 func _on_toggled(toggled_on: bool) -> void:
-	_button_pressed = toggled_on
+	running = toggled_on
 	
-	if toggled_on and not _attacking:
+	if running and not jumping:
 		$CharacterBody2D/AnimatedSprite2D.play("run")
 		$AudioStreamPlayer.play()
 	else:
@@ -18,20 +18,20 @@ func _on_toggled(toggled_on: bool) -> void:
 		$AudioStreamPlayer.stop()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("right"):
-		if not _attacking:
+	if event.is_action_pressed("touche right"):
+		if not jumping:
 			_start_attack()
 		else:
 			_stop_attack()
 
 func _start_attack() -> void:
-	_attacking = true
-	$CharacterBody2D/AnimatedSprite2D.play("right")
+	jumping = true
+	$CharacterBody2D/AnimatedSprite2D.play("jump")
 	$AudioStreamPlayer.play()
 
 func _stop_attack() -> void:
-	_attacking = false
-	if _button_pressed:
+	jumping = false
+	if running:
 		$CharacterBody2D/AnimatedSprite2D.play("run")
 	else:
 		$CharacterBody2D/AnimatedSprite2D.stop()
